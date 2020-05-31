ef118ef619f986af120d39a8bbd043601797d579
demetcaliskan <caliskand@mef.edu.tr>
2020-05-31 23:10:39 +0300

    Initial Commit

---
+++ CelatoFashion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CelatoFashion
+//
+//  Created by Demet on 31.05.2020.
+//  Copyright © 2020 Demet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
